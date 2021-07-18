57122a6ee36daec7f35c77c40c7771db4f4af6bf
smileynet <sambiggins@gmail.com>
2021-07-18 02:09:40 -0700

    Day 3 Part 1

--- 2015/day_3.py
+++ 2015/day_3.py
@@ -1,11 +1,39 @@
-def search_for(dict, lst):
-    return [i for i in lst if all(i[target_key] == target_value for target_key, target_value in dict.items())]
+import utils
 
-dict_to_search_for = {
-    'hobby':'skiing',
-    'job':'doctor'
-}
+class CoordinateTracker:
 
-lst = [{'name':'Mike', 'hobby':'skiing', 'job':'doctor'}, {'name':'Steve', 'hobby':'skiing', 'job':'doctor'}, {'name':'Sally', 'hobby':'chess', 'job':'teacher'} ]
+    def __init__(self):
+        self.coords = []
+        self.current_x = 0
+        self.current_y = 0
+        self.main()
 
-search_for(dict_to_search_for, lst)
+    def check_for_coords(self, x_val, y_val):
+        new_coords = dict(x=x_val, y=y_val)
+        result = [i for i in self.coords if all(i[target_key] == target_value for target_key, target_value in new_coords.items())]
+        return result
+
+    def add_coords(self, x_val, y_val):
+        self.coords.append(dict(x=x_val, y=y_val))
+
+    def process_direction(self, direction_char):
+        if direction_char == ">":
+            self.current_x += 1
+        elif direction_char == "<":
+            self.current_x -= 1
+        elif direction_char == "^":
+            self.current_y += 1
+        elif direction_char == "v":
+            self.current_y -= 1
+        result = self.check_for_coords(self.current_x, self.current_y)
+        if not result:
+            self.add_coords(self.current_x, self.current_y)
+
+    def main(self):
+        input = utils.read_input("day_3_inputs")
+        for character in input:
+            self.process_direction(character)
+        print(len(self.coords))
+
+
+tracker = CoordinateTracker()
