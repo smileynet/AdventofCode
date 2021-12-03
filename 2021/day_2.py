ab19f8f66fc6c9ef788f88296c5d805cb2f3aa8a
smileynet <sambiggins@gmail.com>
2021-12-03 10:40:39 -0800

    2021 day 2 challenge

---
+++ 2021/day_2.py
@@ -0,0 +1,43 @@
+import utils
+
+x = 0
+y = 0
+
+moves = utils.read_input_per_line('day_2_inputs')
+
+# PART 1
+for move in moves:
+  direction, amount = move.split(' ')
+  amount = int(amount)
+
+  if direction == "forward":
+    x += amount
+  elif direction == "up":
+    y -= amount
+  elif direction == "down":
+    y +=amount
+
+outcome = x * y
+
+print(f'outcome: {outcome}')
+
+# PART 2
+aim = 0
+horizontal_pos = 0
+depth = 0
+
+for move in moves:
+  direction, amount = move.split(' ')
+  amount = int(amount)
+
+  if direction == "forward":
+    horizontal_pos += amount
+    depth += aim * amount
+  elif direction == "up":
+    aim -= amount
+  elif direction == "down":
+    aim +=amount
+
+outcome = horizontal_pos * depth
+
+print(f'outcome: {outcome}')
